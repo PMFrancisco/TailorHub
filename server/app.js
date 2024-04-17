abe3333a6280e8754a6b70079395cb774c28ea0e
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-04-17 17:48:22 +0200

    feat(server): logout, add and remove favourites

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const bodyParser= require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 
 require("dotenv").config();
 
-
-const whitelist = process.env.CORS_DOMAINS;
+const whitelist = process.env.CORS_DOMAINS.split(",");
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -24,9 +24,10 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use("/", require("./routes"));
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserModel = require("../models/userModel");
+const authenticateToken = require("../middleware/authenticateToken");
 
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
@@ -9,7 +10,9 @@ router.post("/register", async (req, res) => {
     return res.status(400).json({ error: "All fields are required." });
   }
   if (password.length < 6) {
-    return res.status(400).json({ error: "Password must be at least 6 characters long." });
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 6 characters long." });
   }
 
   try {
@@ -20,25 +23,80 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-        const user = await UserModel.findByEmail(email);
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-
-        const isMatch = await UserModel.validatePassword(user, password);
-        if (!isMatch) {
-            return res.status(401).json({ error: "Invalid credentials" });
-        }
-
-        const token = UserModel.generateToken(user);
-        res.json({ username: user.username, token });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const user = await UserModel.findByEmail(email);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    const isMatch = await UserModel.validatePassword(user, password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = UserModel.generateToken(user);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 3600000,
+    });
+    res.json({ username: user.username, email: user.email, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
+router.get("/user", authenticateToken, async (req, res) => {
+  try {
+    const user = await UserModel.findByEmail(req.user.email);
+    res.json({ username: user.username, email: user.email, user: user.favourites });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve user data" });
+  }
+});
+
+router.post("/logout", authenticateToken, async (req, res) => {
+  try {
+    res.clearCookie("token");
+
+    res.json({ message: "Sesión cerrada exitosamente" });
+  } catch (error) {
+    res.status(500).json({ error: "Error al cerrar sesión" });
+  }
+});
+
+router.post("/favorites/add", authenticateToken, async (req, res) => {
+  const username = req.user.username;
+  const { restaurantId } = req.body;
+
+  try {
+    const user = await UserModel.findByUsername(username);
+    if (!user.favorites.includes(restaurantId)) {
+      user.favorites.push(restaurantId);
+      await user.save();
+    }
+    res.json({ favorites: user.favorites });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/favorites/remove", authenticateToken, async (req, res) => {
+  const username = req.user.username;
+  const { restaurantId } = req.body;
+
+  try {
+    const user = await UserModel.findByUsername(username);
+    user.favorites = user.favorites.filter(id => id !== restaurantId);
+    await user.save();
+    res.json({ favorites: user.favorites });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 module.exports = router;
